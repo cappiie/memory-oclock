41425cd4fb83dbc14c90a46569293b1b38cb588d
cappiie <loicflament62@gmail.com>
2019-11-18 11:23:35 +0100

    Connexion BDD

--- bdd.php
+++ bdd.php
@@ -1,4 +1,3 @@
 <?php
 $options = array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8",PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION);	
-//$bdd = new PDO('mysql:host=localhost;dbname=oclock', 'root','', $options);
-$bdd = new PDO('mysql:host=fl154127-001.privatesql:35193;dbname=Qommunity', 'loicFlament','Cappiie1989', $options);
+$bdd = new PDO('mysql:host=localhost;dbname=oclock', 'root','', $options);
